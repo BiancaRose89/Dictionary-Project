93f864f5acaa2060ac278ee23dd06f7a0c293543
BiancaRose89 <77932470+BiancaRose89@users.noreply.github.com>
2021-07-23 16:42:08 +0100

    Organised photos and added personal links

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.png";
 import "./App.css";
 import Dictionary from "./Dictionary";
 
@@ -7,13 +6,39 @@ export default function App() {
     <div className="App">
       <div className="container">
         <header className="App-header">
-          <img src={logo} className="App-logo img-fluid" alt="logo" />
+          <h1 className="main-heading">English Dictionary</h1>
         </header>
         <main>
           <Dictionary defaultKeyword="wine" />
         </main>
         <footer className="App-footer">
-          <small>Coded by Bianca Schwartz</small>
+          <small>
+            This project was coded by
+            <a
+              href="https://adoring-kepler-a9a12f.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              Bianca Schwartz{" "}
+            </a>
+            and is{" "}
+            <a
+              href="https://github.com/BiancaRose89/weather-react"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              open-sourced on GitHub{" "}
+            </a>
+            and{" "}
+            <a
+              href="https://focused-bardeen-30372a.netlify.app/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              hosted on Netlify{" "}
+            </a>
+          </small>
         </footer>
       </div>
     </div>
